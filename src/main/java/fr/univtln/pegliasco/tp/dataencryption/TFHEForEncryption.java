03c42e792212ee15d23c3f30ad7afde2bf208365
Hermelin_Loïs <hermelin.lois06@gmail.com>
2025-05-06 11:19:52 +0200

    initialisation des methode de chiffrement et de déchiffrement

--- src/main/java/fr/univtln/pegliasco/tp/dataencryption/TFHEForEncryption.java
+++ src/main/java/fr/univtln/pegliasco/tp/dataencryption/TFHEForEncryption.java
@@ -1,5 +1,35 @@
 package fr.univtln.pegliasco.tp.dataencryption;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class TFHEForEncryption {
+    private int[] key = searchPrivateKey();
+
+    /**
+     * Load the native library "tfhe" at runtime. This library contains the native
+     * methods for encryption and decryption.
+     */
+    static {
+        System.loadLibrary("tfhe_encryptor");
+    }
+
+    /**
+     * Retrieves the string key used for encryption from environment variable
+     * 
+     * @return private key convert to int array.
+     */
+    private int[] searchPrivateKey() {
+        String environmentKey = Dotenv.configure().load().get("DB_KEY");
+        char[] keyChars = environmentKey.toCharArray();
+        int[] privateKey = new int[keyChars.length];
+        for (int i = 0; i < keyChars.length; i++) {
+            privateKey[i] = Character.getNumericValue(keyChars[i]);
+        }
+        return privateKey;
+    }
+
+    public native void encrypt();
+
+    public native void decrypt();
 
 }
